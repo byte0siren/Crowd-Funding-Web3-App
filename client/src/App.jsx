54f733ce7923007ce6bd4e70ada4de8184b5ed9e
byte0siren <louisrobinson246@outlook.com>
2022-12-14 12:56:31 +0530

    sidebar completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home, CampaignDetails, CreateCampaign, Profile } from "./pages";
+import { Navbar, Sidebar } from "./components";
 
 const App = () => {
   return (
-    <div>
-      app
-      <div></div>
+    <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="hidden sm:flex mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
